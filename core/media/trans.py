897cb28002c695255647c6227dfb28eb8f4ee61f
raul <meloadik@gmail.com>
2022-12-16 23:41:22 -0600

    webp and galery in index

--- core/media/trans.py
+++ core/media/trans.py
@@ -1,29 +1,23 @@
 from pathlib import Path
 from PIL import Image
-
+import PIL
 
 def convert_to_webp(source):
-    """Convert image to webp.
-
-    Args:
-        source (pathlib.Path): Path to source image
-
-    Returns:
-        pathlib.Path: path to new image
-    """
     destination = source.with_suffix(".webp")
 
     image = Image.open(source)  # Open image
-    image.save(destination, format="webp")  # Convert image to webp
+    base_width = 600
+    width_percent = (base_width / float(image.size[0]))
+    hsize = int((float(image.size[1]) * float(width_percent)))
+    image = image.resize((base_width, hsize), PIL.Image.ANTIALIAS)
+    image.save(destination, format="webp", optimize=True, quality=70)  # Convert image to webp
 
     return destination
 
-
 def main():
-    paths = Path("images").glob("**/*.jpeg")
+    paths = Path("elegidasproductos").glob("**/*.jpg")
     for path in paths:
         webp_path = convert_to_webp(path)
         print(webp_path)
 
-
-main() 
+main()
